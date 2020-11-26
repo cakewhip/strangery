b223118d633956feb7bf53f4381ec1b21ce9151e
Kavin Phan <phan.kavin@gmail.com>
2020-11-26 08:50:43 -0800

    Remove blt add rock candy and reformat

--- src/main/java/com/kqp/strangery/Strangery.java
+++ src/main/java/com/kqp/strangery/Strangery.java
@@ -4,31 +4,16 @@ import com.kqp.strangery.statuseffect.CustomStatusEffect;
 import com.kqp.strangery.statuseffect.HallucinatingStatusEffect;
 import com.kqp.strangery.statuseffect.HealthStatusEffect;
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
-import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
-import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
-import net.fabricmc.fabric.api.tool.attribute.v1.FabricToolTags;
-import net.minecraft.block.Block;
-import net.minecraft.block.Material;
 import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.effect.StatusEffect;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffectType;
 import net.minecraft.entity.effect.StatusEffects;
-import net.minecraft.item.BlockItem;
 import net.minecraft.item.FoodComponent;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
 import net.minecraft.item.Items;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.registry.BuiltinRegistries;
 import net.minecraft.util.registry.Registry;
-import net.minecraft.world.gen.GenerationStep;
-import net.minecraft.world.gen.decorator.Decorator;
-import net.minecraft.world.gen.decorator.RangeDecoratorConfig;
-import net.minecraft.world.gen.feature.ConfiguredFeature;
-import net.minecraft.world.gen.feature.Feature;
-import net.minecraft.world.gen.feature.OreFeatureConfig;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -38,122 +23,20 @@ public class Strangery implements ModInitializer {
 
     @Override
     public void onInitialize() {
-        B.init();
         I.init();
         SE.init();
 
-        FDO.init();
-
-        WF.init();
+        FD.init();
     }
 
     public static Identifier id(String name) {
         return new Identifier(MOD_ID, name);
     }
 
-    // Blocks
-    public static class B {
-        public static final Block FOODIUM_ORE =
-            register(new Block(FabricBlockSettings
-                .of(Material.STONE)
-                .requiresTool()
-                .breakByTool(FabricToolTags.PICKAXES, 2)
-                .strength(3.0F, 3.0F)
-            ), "foodium_ore");
-
-        public static void init() {
-        }
-
-        private static Block register(Block block, String name) {
-            Registry.register(Registry.BLOCK, id(name), block);
-            Registry.register(
-                Registry.ITEM,
-                id(name),
-                new BlockItem(block, new Item.Settings().group(ItemGroup.BUILDING_BLOCKS))
-            );
-
-            return block;
-        }
-    }
-
     // Items
     public static class I {
         public static final Item UNWIELDY_STICK =
-            register(new Item(new Item.Settings().group(ItemGroup.MATERIALS)), "unwieldy_stick");
-
-        public static final Item FOODIUM =
-            register(new Item(new Item.Settings().group(ItemGroup.MATERIALS)), "foodium");
-
-        public static final Item BUN = register(new Item(new Item.Settings()
-            .group(ItemGroup.FOOD)
-            .food(foodComp(2, 0.1F))
-        ), "bun");
-
-        public static final Item CHEESE = register(new Item(new Item.Settings()
-            .group(ItemGroup.FOOD)
-            .food(foodComp(1, 0.0F))
-        ), "cheese");
-
-        public static final Item RICE = register(new Item(new Item.Settings()
-            .group(ItemGroup.FOOD)
-            .food(foodComp(2, 0.1F))
-        ), "rice");
-
-        public static final Item FRENCH_FRIES = register(new Item(new Item.Settings()
-            .group(ItemGroup.FOOD)
-            .food(foodComp(4, 0.1F))
-        ), "french_fries");
-
-        public static final Item CHEESE_BURGER = register(new Item(new Item.Settings()
-            .group(ItemGroup.FOOD)
-            .food(foodComp(12, 0.3F))
-        ), "cheese_burger");
-
-        public static final Item BLT = register(new Item(new Item.Settings()
-            .group(ItemGroup.FOOD)
-            .food(foodComp(10, 0.3F))
-        ), "blt");
-
-        public static final Item ICE_CREAM = register(new Item(new Item.Settings()
-            .group(ItemGroup.FOOD)
-            .food(foodComp(6, 0.1F))
-        ), "ice_cream");
-
-        public static final Item CHICKEN_FRIED_RICE = register(new Item(new Item.Settings()
-            .group(ItemGroup.FOOD)
-            .food(foodComp(12, 0.3F))
-        ), "chicken_fried_rice");
-
-        public static final Item CHICKEN_TENDIES = register(new Item(new Item.Settings()
-            .group(ItemGroup.FOOD)
-            .food(foodComp(10, 0.2F))
-        ), "chicken_tendies");
-
-        public static final Item KOREAN_BBQ = register(new Item(new Item.Settings()
-
-            .group(ItemGroup.FOOD)
-            .food(foodComp(18, 0.5F))
-        ), "korean_bbq");
-
-        public static final Item SUSHI = register(new Item(new Item.Settings()
-            .group(ItemGroup.FOOD)
-            .food(foodComp(18, 0.4F))
-        ), "sushi");
-
-        public static final Item PHO = register(new Item(new Item.Settings()
-            .group(ItemGroup.FOOD)
-            .food(foodComp(16, 0.4F))
-        ), "pho");
-
-        public static final Item RAMEN = register(new Item(new Item.Settings()
-            .group(ItemGroup.FOOD)
-            .food(foodComp(16, 0.25F))
-        ), "ramen");
-
-        public static final Item PIZZA = register(new Item(new Item.Settings()
-            .group(ItemGroup.FOOD)
-            .food(foodComp(18, 0.25F))
-        ), "pizza");
+            register(new Item(new Item.Settings()), "unwieldy_stick");
 
         public static void init() {
         }
@@ -162,13 +45,6 @@ public class Strangery implements ModInitializer {
             Registry.register(Registry.ITEM, id(name), item);
 
             return item;
-        }
-
-        private static FoodComponent foodComp(int hunger, float saturation) {
-            return new FoodComponent.Builder()
-                .hunger(hunger)
-                .saturationModifier(saturation)
-                .build();
         }
     }
 
@@ -213,11 +89,10 @@ public class Strangery implements ModInitializer {
         }
     }
 
-    // Food data overrides
-    public static class FDO {
+    // Food data
+    public static class FD {
         private static final Map<Identifier, FoodComponent>
             ITEM_FOOD_COMPONENT_MAP = new HashMap<Identifier, FoodComponent>();
-
         private static final FoodComponent FAUNA = (new FoodComponent.Builder())
             .hunger(1)
             .saturationModifier(0.1F)
@@ -347,35 +222,6 @@ public class Strangery implements ModInitializer {
 
         private static void add(Item item, FoodComponent foodComponent) {
             ITEM_FOOD_COMPONENT_MAP.put(Registry.ITEM.getId(item), foodComponent);
-        }
-    }
-
-    public static class WF {
-        private static final ConfiguredFeature<?, ?> FOODIUM_ORE_OVERWORLD = Feature.ORE
-            .configure(new OreFeatureConfig(
-                OreFeatureConfig.Rules.BASE_STONE_OVERWORLD,
-                B.FOODIUM_ORE.getDefaultState(),
-                8
-            ))
-            .decorate(Decorator.RANGE.configure(new RangeDecoratorConfig(
-                0,
-                0,
-                64
-            )))
-            .spreadHorizontally()
-            .repeat(12);
-
-        public static void init() {
-            Registry.register(
-                BuiltinRegistries.CONFIGURED_FEATURE,
-                id("foodium_ore"),
-                FOODIUM_ORE_OVERWORLD
-            );
-            BiomeModifications.addFeature(
-                BiomeSelectors.foundInOverworld(),
-                GenerationStep.Feature.UNDERGROUND_ORES,
-                BuiltinRegistries.CONFIGURED_FEATURE.getKey(FOODIUM_ORE_OVERWORLD).get()
-            );
         }
     }
 }
