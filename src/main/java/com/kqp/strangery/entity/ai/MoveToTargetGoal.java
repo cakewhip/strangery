733692f6d06118e7a6874543f2b7d42cff07ea55
cakewhip <phan.kavin@gmail.com>
2020-11-27 18:58:53 -0500

    Fix kidnapping mechanic

--- src/main/java/com/kqp/strangery/entity/ai/MoveToTargetGoal.java
+++ src/main/java/com/kqp/strangery/entity/ai/MoveToTargetGoal.java
@@ -1,13 +1,33 @@
 package com.kqp.strangery.entity.ai;
 
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.ai.goal.MoveToTargetPosGoal;
 import net.minecraft.entity.mob.PathAwareEntity;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.WorldView;
 
+// TODO this doesn't make sense as a sub class of MoveToTargetPosGoal
 public class MoveToTargetGoal extends MoveToTargetPosGoal {
     public MoveToTargetGoal(PathAwareEntity mob, double speed, int range) {
         super(mob, speed, range);
+    }
+
+    @Override
+    public boolean canStart() {
+        this.cooldown = this.getInterval(this.mob);
+        return this.findTargetPos();
+    }
+
+    @Override
+    public void tick() {
+        LivingEntity target = this.mob.getTarget();
+
+        this.mob.getNavigation().startMovingTo(
+            target.getX(),
+            target.getY(),
+            target.getZ(),
+            this.speed
+        );
     }
 
     @Override
@@ -17,7 +37,7 @@ public class MoveToTargetGoal extends MoveToTargetPosGoal {
 
     @Override
     public boolean shouldContinue() {
-        return this.mob.getTarget() != null && super.shouldContinue();
+        return this.mob.getTarget() != null;
     }
 
     @Override
--- src/main/java/com/kqp/strangery/entity/mob/EnderAgentEntity.java
+++ src/main/java/com/kqp/strangery/entity/mob/EnderAgentEntity.java
@@ -2,9 +2,16 @@ package com.kqp.strangery.entity.mob;
 
 import com.kqp.strangery.Strangery;
 import com.kqp.strangery.entity.ai.MoveToTargetGoal;
-import net.minecraft.entity.*;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityData;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.SpawnReason;
 import net.minecraft.entity.ai.TargetPredicate;
-import net.minecraft.entity.ai.goal.*;
+import net.minecraft.entity.ai.goal.FleeEntityGoal;
+import net.minecraft.entity.ai.goal.LookAroundGoal;
+import net.minecraft.entity.ai.goal.LookAtEntityGoal;
+import net.minecraft.entity.ai.goal.WanderAroundFarGoal;
 import net.minecraft.entity.ai.pathing.PathNodeType;
 import net.minecraft.entity.attribute.DefaultAttributeContainer;
 import net.minecraft.entity.attribute.EntityAttributes;
@@ -14,6 +21,7 @@ import net.minecraft.entity.passive.AnimalEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.sound.SoundEvent;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.LocalDifficulty;
 import net.minecraft.world.ServerWorldAccess;
 import net.minecraft.world.World;
@@ -22,6 +30,10 @@ import org.jetbrains.annotations.Nullable;
 import java.util.List;
 
 public class EnderAgentEntity extends HostileEntity {
+    private static final int PLAYER_FLEE_DISTANCE = 16;
+    private static final int KIDNAP_DISTANCE = 32;
+    private static final int KIDNAP_PLAYER_FLEE_DISTANCE = 48;
+
     private static final TargetPredicate ANIMAL_TARGET_PREDICATE = new TargetPredicate()
         .setPredicate((animal) -> !(animal.getVehicle() instanceof EnderAgentEntity));
 
@@ -29,6 +41,7 @@ public class EnderAgentEntity extends HostileEntity {
         super(type, world);
 
         this.setPathfindingPenalty(PathNodeType.WATER, -1.0F);
+        this.stepHeight = 1.6F;
     }
 
     @Override
@@ -58,45 +71,90 @@ public class EnderAgentEntity extends HostileEntity {
 
     @Override
     protected void initGoals() {
-        this.goalSelector.add(0, new FleeEntityGoal<PlayerEntity>(this, PlayerEntity.class, 8.0F, 1.0D, 1.0D));
-        this.goalSelector.add(1, new MoveToTargetGoal(this, 1.0D, 32));
-        this.goalSelector.add(2, new LookAtEntityGoal(this, AnimalEntity.class, 8.0F));
-        this.goalSelector.add(3, new LookAroundGoal(this));
-        this.goalSelector.add(4, new WanderAroundFarGoal(this, 1.0D));
+        this.goalSelector.add(0, new FleeEntityGoal<PlayerEntity>(
+            this,
+            PlayerEntity.class,
+            PLAYER_FLEE_DISTANCE,
+            1.15D,
+            1.15D
+        ));
+        this.goalSelector.add(1, new MoveToTargetGoal(
+            this,
+            1.15D,
+            KIDNAP_DISTANCE
+        ));
+        this.goalSelector.add(2, new FleeEntityGoal<PlayerEntity>(
+            this,
+            PlayerEntity.class,
+            // Ender agent kills kidnapped animal once at this range, so must flee beyond
+            KIDNAP_PLAYER_FLEE_DISTANCE + 16,
+            1D,
+            1D
+        ));
+        this.goalSelector.add(3, new LookAtEntityGoal(this, AnimalEntity.class, 8.0F));
+        this.goalSelector.add(4, new LookAroundGoal(this));
+        this.goalSelector.add(5, new WanderAroundFarGoal(this, 1.0D));
     }
 
     @Override
     public void tickMovement() {
-        if (!this.hasPassengers()) {
-            List<AnimalEntity> animals = this.world.<AnimalEntity>getEntitiesByClass(
-                AnimalEntity.class,
-                this.getBoundingBox().expand(0.25D, 0.0D, 0.25D),
-                (animal) -> !(animal.getVehicle() instanceof EnderAgentEntity)
-            );
+        // TODO maybe move this to a target goal class
+        if (!world.isClient && age % 10 == 0) {
+            if (!this.hasPassengers()) {
+                LivingEntity target = this.getTarget();
 
-            for (AnimalEntity animal : animals) {
-                animal.startRiding(this, true);
-                this.setTarget(null);
-                break;
+                if (target != null) {
+                    if (this.distanceTo(target) < 1.25D) {
+                        target.startRiding(this, true);
+                        this.setTarget(null);
+                    }
+                } else if (target == null || this.distanceTo(target) > KIDNAP_DISTANCE) {
+                    this.setTarget(this.world.getClosestEntityIncludingUngeneratedChunks(
+                        AnimalEntity.class,
+                        ANIMAL_TARGET_PREDICATE,
+                        this,
+                        this.getX(),
+                        this.getEyeY(),
+                        this.getZ(),
+                        this.getBoundingBox().expand(48D)
+                    ));
+                }
             }
-        } else {
-            this.setTarget(this.world.getClosestEntityIncludingUngeneratedChunks(
-                AnimalEntity.class,
-                ANIMAL_TARGET_PREDICATE,
-                this,
-                this.getX(),
-                this.getEyeY(),
-                this.getZ(),
-                this.getBoundingBox().expand(48D)
-            ));
         }
 
         super.tickMovement();
     }
 
+    @Override
+    public void baseTick() {
+        super.baseTick();
+
+        if (this.hasPassengers()) {
+            PlayerEntity player = world.getClosestPlayer(
+                this.getX(),
+                this.getY(),
+                this.getZ(),
+                KIDNAP_PLAYER_FLEE_DISTANCE,
+                true
+            );
+
+            if (player == null) {
+                for (Entity passenger : this.getPassengerList()) {
+                    if (passenger instanceof LivingEntity) {
+                        passenger.damage(
+                            DamageSource.MAGIC,
+                            Float.MAX_VALUE
+                        );
+                    }
+                }
+            }
+        }
+    }
 
     @Override
-    public @Nullable EntityData initialize(ServerWorldAccess world, LocalDifficulty difficulty, SpawnReason spawnReason, @Nullable EntityData entityData, @Nullable CompoundTag entityTag) {
+    public @Nullable EntityData initialize(ServerWorldAccess world, LocalDifficulty difficulty,
+                                           SpawnReason spawnReason, @Nullable EntityData entityData,
+                                           @Nullable CompoundTag entityTag) {
         this.initEquipment(difficulty);
 
         return super.initialize(world, difficulty, spawnReason, entityData, entityTag);
@@ -104,10 +162,10 @@ public class EnderAgentEntity extends HostileEntity {
 
     public static DefaultAttributeContainer.Builder createEnderAgentAttributes() {
         return HostileEntity.createHostileAttributes()
-                .add(EntityAttributes.GENERIC_FOLLOW_RANGE, 48.0D)
-                .add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 0.365D)
-                .add(EntityAttributes.GENERIC_ATTACK_DAMAGE, 0.5D)
-                .add(EntityAttributes.GENERIC_MAX_HEALTH, 10.0D);
+            .add(EntityAttributes.GENERIC_FOLLOW_RANGE, 48.0D)
+            .add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 0.365D)
+            .add(EntityAttributes.GENERIC_ATTACK_DAMAGE, 0.5D)
+            .add(EntityAttributes.GENERIC_MAX_HEALTH, 10.0D);
     }
 
     @Override
