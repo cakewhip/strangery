6070ffa7a06d07840186989f9a4be98e7b2df2d2
cakewhip <phan.kavin@gmail.com>
2020-11-27 20:19:57 -0500

    Change sparring damage to redirect implementation

--- src/main/java/com/kqp/strangery/mixin/SparringDamageAdder.java
+++ src/main/java/com/kqp/strangery/mixin/SparringDamageAdder.java
@@ -1,6 +1,6 @@
 package com.kqp.strangery.mixin;
 
-import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
@@ -8,31 +8,35 @@ import net.minecraft.item.ToolItem;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 /**
  * Removes damage done by non-tools.
  */
-@Mixin(LivingEntity.class)
+@Mixin(PlayerEntity.class)
 public class SparringDamageAdder {
     private static final float LOW_DAMAGE = 0.000001F;
 
-    // TODO this implementation sucks, since bows don't work and what not
-    //      it would work a lot better to inject in the player class itself
-    @Inject(method = "damage", at = @At("HEAD"), cancellable = true)
-    private void injectDamage(DamageSource source, float amount,
-                              CallbackInfoReturnable<Boolean> callbackInfo) {
+    @Redirect(
+        method = "attack",
+        at = @At(
+            value = "INVOKE",
+            target = "Lnet/minecraft/entity/Entity;damage(Lnet/minecraft/entity/damage/DamageSource;F)Z"
+        )
+    )
+    private boolean redirectDamage(Entity target, DamageSource source, float amount) {
         if (amount > LOW_DAMAGE) {
             if (source.getAttacker() != null && source.getAttacker() instanceof PlayerEntity) {
                 PlayerEntity attacker = (PlayerEntity) source.getAttacker();
                 ItemStack stack = attacker.getMainHandStack();
 
                 if (stack.isEmpty() || !(stack.getItem() instanceof ToolItem)) {
-                    callbackInfo.setReturnValue(
-                        ((LivingEntity) (Object) this).damage(source, LOW_DAMAGE)
-                    );
+                    amount = LOW_DAMAGE;
                 }
             }
         }
+
+        return target.damage(source, amount);
     }
 }
