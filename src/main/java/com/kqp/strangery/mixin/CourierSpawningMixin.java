8fcf3eb957754692f01f8c4f6532fc6bba3fc5f2
cakewhip <phan.kavin@gmail.com>
2020-12-13 18:15:43 -0500

    Adjust courier spawn rate

--- src/main/java/com/kqp/strangery/mixin/CourierSpawningMixin.java
+++ src/main/java/com/kqp/strangery/mixin/CourierSpawningMixin.java
@@ -20,7 +20,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(LivingEntity.class)
 public class CourierSpawningMixin {
 
-    private static final float HATE_MAIL_COURIER_CHANCE = 0.05F;
+    private static final float HATE_MAIL_COURIER_CHANCE = 0.005F;
 
     private static final Random RANDOM = new Random();
 
